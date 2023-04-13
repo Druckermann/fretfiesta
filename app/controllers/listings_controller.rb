cbdce9db90962fcb7f3bbdb2d3a055d85df20e41
samira hossain <samira.hossain@outlook.com>
2023-04-13 21:48:41 +1000

    Listings and Bookings Controllers, Bookings Views updated

--- app/controllers/listings_controller.rb
+++ app/controllers/listings_controller.rb
@@ -5,19 +5,18 @@ class ListingsController < ApplicationController
 
   def show
     @listing = Listing.find(params[:id])
-    @bookmark = Bookmark.new
   end
 
   def new
-    @listing = listing.new
+    @listing = Listing.new
   end
 
   def create
-    @listing = listing.new(listing_params)
+    @listing = Listing.new(listing_params)
     if @listing.save
       redirect_to @listing
+    else
+      render :new, status: :unprocessable_entity
     end
-   # else
-     # render :new, status: :unprocessable_entity
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -6,4 +6,10 @@ Rails.application.routes.draw do
   # Defines the root path route ("/")
   # root "articles#index"
   resources :listings, only: %i[index show new create]
+  resources :bookings, only: %i[index show new create] do
+  # approval booking route
+    member do
+      patch :update
+    end
+  end
 end
