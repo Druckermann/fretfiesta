a83be4570db82c0c1934abf673b74ffe2f25b9f6
Allan Turton <forshopping50@hotmail.com>
2023-04-15 12:46:07 +1000

    Still working through bookings views

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -2,4 +2,10 @@ class Booking < ApplicationRecord
   belongs_to :user
   belongs_to :listing
   validates :rent_start, :rent_end, :total_price, :status, presence: true
+
+  # This bits marks a booking as pending
+  def pending?
+    status == 'pending'
+  end
+
 end
